34071fad94a1edd12c704997e1df0ddafe7cd0a6
Caleb Anderson <caleb.anderson130@gmail.com>
2017-06-13 16:57:05 -0500

    Change port for production

--- client/models/channels.js
+++ client/models/channels.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch'
 
 export function fetchAllChannels() {
-  return fetch('http://localhost:4000/channels')
+  return fetch('http://159.203.103.19:4000/channels')
     .then(data => data.json())
 }
